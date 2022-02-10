d779e255d514aa470a5c9a4e3bf376f818260164
CristianoLee <839658551@qq.com>
2022-02-11 01:20:55 +0800

    更新了学习进度

---
+++ node.js/day5/08.router.js
@@ -0,0 +1,17 @@
+// 这是路由模块
+// 1.导入 express
+const express = require('express')
+// 2.创建路由对象
+const router = express.Router()
+
+// 挂载具体的路由
+router.get('/user/list', (req, res) => {
+    res.send('get user list')
+})
+router.post('/user/add', (req, res) => {
+    res.send('add new user')
+})
+
+
+// 4.向外导出路由对象
+module.exports = router
