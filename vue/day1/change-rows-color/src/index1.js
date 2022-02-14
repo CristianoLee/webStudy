94bb0f8af71db4b609e52d83951c826c9ddbe76f
CristianoLee <839658551@qq.com>
2022-02-15 05:54:39 +0800

    更新了学习进度

--- vue/day1/change-rows-color/src/index1.js
+++ vue/day1/change-rows-color/src/index1.js
@@ -1,11 +1,35 @@
 // 1.使用ES6 导入语法，导入jquery
 import $ from 'jquery'
+// 导入样式{在 webpack 中，一切皆模块，都可以通过 es6 语法导入}
+// 如果某个模块中，使用 from 接收到的成员为 undefined，则没必要接收
+import '@/css/index.css'
+import '@/css/index.less'
+
+// 导入 src/js/test/info.js
+import '@/js/test/info'
+
+// 导入图片，得到图片文件
+import logo from '@/images/logo.jpg'
+console.log(logo);
+// 给 img 标签的 src 动态赋值
+$('.box').attr('src', logo)
+
 
 // 2.定义jquery入口函数
 $(function () {
     // 3.实现奇偶行变色
-    $('li:odd').css('background-color', 'cyan')
+    $('li:odd').css('background-color', 'red')
     $('li:even').css('background-color', 'pink')
 
 })
+// 定义装饰器函数
+function info(target) {
+    target.info = 'Person info'
+}
+
+// 定义一个普通类
+@info
+class Person { }
+
+console.log(Person.info);
 
--- vue/day1/change-rows-color/webpack.config.js
+++ vue/day1/change-rows-color/webpack.config.js
@@ -11,8 +11,15 @@ const htmlPlugin = new HtmlPlugin({
     filename: './index.html'
 })
 
+// 注意：左侧的花括号{}是解构赋值
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 // 使用node.js 中的导出语法，向外导出一个 webpack 的配置对象
 module.exports = {
+    // 在开发调试阶段，建议大家都把 devtool 的值设置为 eval-source-map
+    // devtool: 'eval-source-map',
+    // 在实际发布的时候，建议大家把 devtool 的值设置为 nosources-source-map 或直接关闭 SourceMap
+    devtool: 'nosources-source-map',
     // 代表 webpack 运行的模式，可选值有两个 development 和 production
     // 结论：开发时候一定要用development因为追求的是打包速度而不是体积
     // 发布上线时一定要用production,因为上线追求的是体积小，而不是打包速度快！
@@ -24,10 +31,10 @@ module.exports = {
         // 存放目录
         path: path.join(__dirname, 'dist'),
         // 生成的文件名
-        filename: 'bundle.js'
+        filename: 'js/bundle.js'
     },
     // 插件的数组，将来webpack在运行时，会加载并调用这些插件
-    plugins: [htmlPlugin],
+    plugins: [htmlPlugin, new CleanWebpackPlugin()],
     devServer: {
         // 首次打包成功后，自动打开浏览器进入默认网页
         open: true,
@@ -35,5 +42,27 @@ module.exports = {
         port: 80,
         // 指定运行的主机地址
         host: '127.0.0.1'
+    },
+    module: {
+        rules: [
+            // 定义了不同模块对应的 loader
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            // 处理 .less 文件的 loader
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+            // 处理图片文件的 loader
+            // 如需要调用的 loader 只有一个，则只传递一个字符串就行，如果有多个 loader，必须指定数组
+            // 在配置 url-loader 的时候，多个参数之间，使用 & 符号进行分隔
+            { test: /\.jpg|png|gif$/, use: 'url-loader?limit=470&outputPath=images' },
+            // 使用 babel-loader 处理高级的 JS 语法
+            // 在配置 babel-loader 的时候，程序员只需要把自己的代码进行转换即可；一定要排除 node_modules 目录中的 JS 文件
+            // 因为第三方包中的 JS 兼容性，不需要程序员关心
+            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ }
+        ]
+    },
+    resolve: {
+        alias: {
+            // 告诉 webpack，程序员写的代码中，@ 符号表示 src 这一层目录
+            '@': path.join(__dirname, './src')
+        }
     }
 }
